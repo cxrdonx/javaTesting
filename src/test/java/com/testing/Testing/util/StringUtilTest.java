6ca2dd59c63d346ba2c8e88a068e0d9f50495d42
José Cardona <jose.eduardo.cardona@gmail.com>
2022-01-04 22:41:26 -0600

    organizacion de test, un test por cada prueba

--- src/test/java/com/testing/Testing/util/StringUtilTest.java
+++ src/test/java/com/testing/Testing/util/StringUtilTest.java
@@ -1,9 +1,41 @@
 package com.testing.Testing.util;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.awt.*;
+
 public class StringUtilTest {
-    public static void main(String[] args) {
-        String result = StringUtil.repeat("HOLA", 10);
-        System.out.println( result);
+    @Test
+    public void repeat_string_multiple_times(){
+        Assert.assertEquals("holaholahola", StringUtil.repeat("hola", 3));
     }
+
+    @Test
+    public void repeat_string_two_times(){
+        Assert.assertEquals("holahola", StringUtil.repeat("hola", 2 ));
+    }
+
+    @Test
+    public void repeat_string_zero_times(){
+        Assert.assertEquals("", StringUtil.repeat("hola", 0));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void repeat_String_negative_times(){
+        StringUtil.repeat("hola", -1);
+    }
+
+
+
+
+    /*
+    private static void assertEquals(String actual, String expected) {
+        if(!actual.equals(expected)){
+            throw new RuntimeException(actual +" is not equal to " + expected);
+        }
+    }
+
+     */
 
 }
